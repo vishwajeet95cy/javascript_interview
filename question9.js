ed21b0a86df3d137bd12a52efa6397892a03f14f
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-30 22:31:40 +0530

    update: Question 9 done

--- question9.js
+++ question9.js
@@ -10,6 +10,23 @@ function palindromNumber(val) {
   }
 }
 
-console.log(palindromNumber("madam"));
-console.log(palindromNumber(1881));
-console.log(palindromNumber(1841));
+function palindromNumber2(val) {
+  const revStrr = val.toString().split("");
+  let value = "";
+
+  for (let i = revStrr.length - 1; i >= 0; i--) {
+    value += revStrr[i];
+  }
+
+  console.log({ val, value });
+
+  if (value == val) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(palindromNumber2("madam"));
+console.log(palindromNumber2(1881));
+console.log(palindromNumber2(1841));
