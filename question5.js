e7390de15fa9de253e170d866c16e800042295e0
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-09-30 21:51:18 +0530

    feat: Question 5 done

--- question5.js
+++ question5.js
@@ -1,1 +1,15 @@
 //How to find Factorial(n!) of a given number?
+
+function factorialNumber(value) {
+  if (value < 0) {
+    console.log(`Factorial of ${value} is not possible`);
+    return;
+  }
+  let val = 1;
+  for (let i = 1; i <= value; i++) {
+    val *= i;
+  }
+  console.log(`factorial of ${value} is ${val}`);
+}
+
+factorialNumber(5);
