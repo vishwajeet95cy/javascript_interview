696cb82b1c331f0af1ed1f66ab40f263bcfa6da6
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-10-01 19:16:03 +0530

    update: question 10 done

--- question10.js
+++ question10.js
@@ -4,6 +4,7 @@ const num1 = 1;
 const num2 = 2;
 const operator = "+";
 
+// if else
 function calculator(num1, num2, operator) {
   let result;
   if (operator == "+") {
@@ -18,7 +19,26 @@ function calculator(num1, num2, operator) {
   return result;
 }
 
+// switch
+function calculatorSwitch(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    default:
+      return num1 / num2;
+  }
+}
+
 console.log(calculator(1, 2, "+"));
 console.log(calculator(1, 2, "-"));
 console.log(calculator(1, 2, "*"));
 console.log(calculator(1, 2, "/"));
+
+console.log(calculatorSwitch(1, 2, "+"));
+console.log(calculatorSwitch(1, 2, "-"));
+console.log(calculatorSwitch(1, 2, "*"));
+console.log(calculatorSwitch(1, 2, "/"));
