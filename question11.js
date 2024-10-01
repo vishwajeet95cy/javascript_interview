872f2ef20f0bda6e7a86187c4b85c03e68943d0e
Vishwajeet Kumar <vishwajeet95cy@gmail.com>
2024-10-01 19:24:49 +0530

    update: question 11 done

--- question11.js
+++ question11.js
@@ -1,5 +1,6 @@
 // How to find number of occurences of a character in a string?
 
+// simple case
 function occurenceCount(value, string) {
   const strValue = value.toString().split("");
   let count = 0;
@@ -12,4 +13,17 @@ function occurenceCount(value, string) {
   return count;
 }
 
-console.log(occurenceCount("This is Hello world", "l"));
+// upper and lower case
+function occurenceCountCase(value, string) {
+  const strValue = value.toString().toLocaleLowerCase().split("");
+  let count = 0;
+  for (let i = 0; i < strValue.length; i++) {
+    if (strValue[i] == string.toLocaleLowerCase()) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(occurenceCount("This is HeLLo world", "L"));
+console.log(occurenceCountCase("This is HeLLo world", "L"));
